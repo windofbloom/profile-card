9b6bb77b9cfca77eae4aca5fb339c9561a87620c
Bloom <bloomillus@gmail.com>
2024-05-11 20:50:31 -0500

    Unistalled HugeIcons, changed for svg files instead with their respective componets. Adding social media icons

--- src/componets/Buttons/Buttons.jsx
+++ src/componets/Buttons/Buttons.jsx
@@ -7,12 +7,10 @@ function Buttons () {
     return(
         <div className='Button-Container'>
             <a href='https://www.behance.net/gallery/197509895/UXUI-Designer-Portfolio' className='Portfolio-button button_uxui'>
-                UX/UI Portfolio 
-                <BookmarkIcon />
+                UX/UI Portfolio <BookmarkIcon color={"white"} size={25} />
             </a>
             <a href='https://cara.app/windofbloom' className='Portfolio-button button_art'>
-               Art Portfolio 
-               <ArtIcon />
+               Art Portfolio <ArtIcon color={"white"} size={25} />
             </a>
         </div>
     )
--- src/componets/Socials/Socials.jsx
+++ src/componets/Socials/Socials.jsx
@@ -1,14 +1,36 @@
-import { FigmaIcon } from '@hugeicons/react-pro';
+import { BehanceIcon } from '../Icons/BehanceIcon';
+import { FigmaIcon } from '../Icons/FigmaIcon';
+import { FiverIcon } from '../Icons/FiverIcons';
+import { GithubIcon } from '../Icons/GithubIcon';
+import { InstagramIcon } from '../Icons/InstagramIcon';
 import './socials.css'
+
 
 function Socials () {
     
     return(
-        <span className='Socials'>
-            <FigmaIcon />
-        </span>
+        <div className='Socials'>
+            
+            <a href="">
+                <FigmaIcon color={"#092147"} size={40}/>
+            </a>
+            <a href="">
+                <BehanceIcon color={"#092147"} size={50} />
+            </a>
+            <a href="">
+                <FiverIcon color={"#092147"} size={40} />
+            </a>
+            <a href="">
+                <GithubIcon color={"#092147"} size={40} />
+            </a>
+            <a href="">
+               <InstagramIcon color={"#092147"} size={40} /> 
+
+            </a>
+        </div>
     )
 
 }
 
 export { Socials };
+
