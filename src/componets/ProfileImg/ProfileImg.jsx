3e415cf60cf1ae83d2698f17da4920e86d2dfe95
Bloom <bloomillus@gmail.com>
2024-05-12 17:37:49 -0500

    Fixex sroll behavior and imags

--- src/componets/ProfileImg/ProfileImg.jsx
+++ src/componets/ProfileImg/ProfileImg.jsx
@@ -1,10 +1,10 @@
 import './profileimg.css'
-import tarnishedoc1 from '../../assets/imgs/tarnishedoc1.jpg';
+import tarnishedoc1 from '../../assets/imgs/tarnishedoc1.png';
 
 function ProfileImg() {
     return (
         <div>
-            <img className='profile__img' src={tarnishedoc1} alt="Profile" />
+            <img className='profile__img' src={tarnishedoc1} loading='lazy' alt="Profile" />
         </div>
     );
 }
