8e3f2fea9ddb8abb812459c94d347270b0804174
Bloom <bloomillus@gmail.com>
2024-06-01 12:16:48 -0500

    Add typing animation to title with css

--- src/componets/Info/Info.jsx
+++ src/componets/Info/Info.jsx
@@ -1,15 +1,18 @@
+
 import './info.css'
 
-function Info () {
-
-    return(
-        <div className='Info-Container'>
-            <h1 className='Info-Container__title'>Bloom Illus</h1>
-            <h2 className='Info-Container__subtitle'>@windofbloom</h2>
-            <p className='Info-Container__text'>UX/UI Designer and Front-End Jr. | Hobby Artist | ESP&ENG</p>
+function Info() {
+    return (
+      <div className="Info-Container">
+        <div className="text-container">
+            <h1 className="Info-Container__title">
+              Bloom Illus
+            </h1>
         </div>
-    )
-
+        <h2 className="Info-Container__subtitle">@windofbloom</h2>
+        <p className="Info-Container__text">UX/UI Designer and Front-End Jr. | Hobby Artist | ESP&ENG</p>
+      </div>
+    );
 }
 
 export { Info };
